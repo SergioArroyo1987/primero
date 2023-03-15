239add6851b56e1760b894feb4314252419f9e5d
sergio Arroyo <chupi1987@hotmail.com>
2023-03-15 13:14:54 +0100

    quitar suma

--- cuadrado.js
+++ cuadrado.js
@@ -1,4 +1,50 @@
+let calculadora1=(a,b,c=0)=>{
+    switch (a){
+        case "sum":
+            return b+c;
+            break;
+        case "subs":
+            return b-c;
+            break;
+        case "mult":
+            return b*c;
+            break;
+        case "div":
+            return b/c;
+            break;
+        default:
+            console.log("Parametros no reconocidos");
+            break;
 
+    }
+
+    }
+    
+console.log("suma"+calculadora1("sum",2,7));
+console.log("resta"+calculadora("subs",2,2));
+console.log("multiplicacion"+calculadora("mult",2));
+console.log("division"+calculadora("div",2,2));
+function calculadora(a,b,c=0){
+    switch (a){
+        
+        case "subs":
+            return b-c;
+            break;
+        case "mult":
+            return b*c;
+            break;
+        case "div":
+            return b/c;
+            break;
+        default:
+            console.log("Parametros no reconocidos");
+            break;
+
+    }
+
+    }
+    let w=["sum",7,1];
+    console.log(calculadora(...w));
 function cuadrado(n){
     console.log(n*n);
 }
